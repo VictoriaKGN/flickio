e35d01919a3b416db11080eb6f98c08ef9ee1174
Victoria Kogan <koganv@myumanitoba.ca>
2025-03-26 10:42:02 -0400

    actions buttons added to movie detail page

--- lib/views/movie_detail/movie_detail_desktop.dart
+++ lib/views/movie_detail/movie_detail_desktop.dart
@@ -108,7 +108,51 @@ class MovieDetailDesktop extends StatelessWidget {
 
                   SizedBox(height: 35),
 
-                  
+                  Wrap(
+                    spacing: 30,
+                    runSpacing: 10,
+                    children: [
+                      // watchlist button
+                      FilledButton.icon(
+                        onPressed: () {},
+                        icon: const Icon(Icons.add),
+                        label: Text("Add to Watchlist".toUpperCase()),
+                        style: FilledButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 18,
+                          ),
+                          textStyle: const TextStyle(
+                            fontSize: 15,
+                            // fontWeight: 
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4),
+                          ),
+                        ),
+                      ),
+
+                      // watched button
+                      FilledButton.icon(
+                        onPressed: () {},
+                        icon: const Icon(Icons.check),
+                        label: Text("Mark as Watched".toUpperCase()),
+                        style: FilledButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 18,
+                          ),
+                          textStyle: const TextStyle(
+                            fontSize: 15,
+                            // fontWeight: 
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
